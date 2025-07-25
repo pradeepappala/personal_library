9fc43f98cdc52338df67d24d1b48551c1ccadb41
pradeepappala <pradeep.appala@gmail.com>
2025-07-25 20:11:34 +0530

    created ui using kviy, updated README verified on android app

--- README.md
+++ README.md
@@ -31,3 +31,23 @@ python -m main
 open workspace in vs code
 ctrl+shift+p select python from the venv created (library_venv\Scripts\python.exe)
 Run (F5/ctrl+F5)
+
+
+# build apk using buildozer
+create wsl (windows subsystem for linux)
+create venv 
+https://www.youtube.com/watch?v=6gNpSuE01qE&t=14s
+
+https://buildozer.readthedocs.io/en/latest/installation.html
+
+https://buildozer.readthedocs.io/en/latest/quickstart.html
+
+# steps followed
+open wsl (ubuntu)
+cd GitHub\my-finance
+source GitHub\my-finance\finance_venv_wsl\bin\activate
+cd GitHub\my-finance\my_android_app
+Edit the buildozer.spec according to the Specifications. You should at least change the title, package.name and package.domain in the [app] section.
+buildozer -v android debug
+apk will be available in my_android_app\bin
+copy apk to mobile and install
--- main.py
+++ main.py
@@ -1,39 +1,288 @@
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from src.personal_library import PersonalLibrary
 
+library = PersonalLibrary()
+
+class MainMenu(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        buttons = [
+            ("Add Book", "add_book"),
+            ("Remove Book", "remove_book"),
+            ("Show All Books", "show_all_books"),
+            ("Show Available Books", "show_available_books"),
+            ("Add Lender", "add_lender"),
+            ("Remove Lender", "remove_lender"),
+            ("Borrow Book", "borrow_book"),
+            ("Return Book", "return_book"),
+            ("Get Borrowed Books with Lender details", "borrowed_books_lender"),
+            ("Get Books Not Borrowed", "books_not_borrowed"),
+        ]
+        for text, screen in buttons:
+            btn = Button(text=text)
+            btn.bind(on_release=lambda btn, s=screen: setattr(self.manager, 'current', s))
+            layout.add_widget(btn)
+        self.add_widget(layout)
+
+class AddBookScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.title_input = TextInput(hint_text='Title')
+        self.author_input = TextInput(hint_text='Author')
+        add_btn = Button(text='Add Book')
+        self.result = Label(text='')
+        add_btn.bind(on_release=self.add_book)
+        layout.add_widget(self.title_input)
+        layout.add_widget(self.author_input)
+        layout.add_widget(add_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def add_book(self, instance):
+        title = self.title_input.text
+        author = self.author_input.text
+        if title and author:
+            book_id = library.add_book(title, author)
+            self.result.text = f"Book added with ID: {book_id}"
+        else:
+            self.result.text = "Please enter both title and author."
+
+class RemoveBookScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.book_id_input = TextInput(hint_text='Book ID')
+        remove_btn = Button(text='Remove Book')
+        self.result = Label(text='')
+        remove_btn.bind(on_release=self.remove_book)
+        layout.add_widget(self.book_id_input)
+        layout.add_widget(remove_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def remove_book(self, instance):
+        try:
+            book_id = int(self.book_id_input.text)
+            library.remove_book(book_id)
+            self.result.text = f"Book {book_id} removed."
+        except Exception as e:
+            self.result.text = str(e)
+
+class ShowAllBooksScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.result = Label(text='')
+        show_btn = Button(text='Show All Books')
+        show_btn.bind(on_release=self.show_books)
+        layout.add_widget(show_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def show_books(self, instance):
+        books = library.get_all_books()
+        self.result.text = '\n'.join([f"{b[0]}: {b[1]} by {b[2]}" for b in books])
+
+class ShowAvailableBooksScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.result = Label(text='')
+        show_btn = Button(text='Show Available Books')
+        show_btn.bind(on_release=self.show_books)
+        layout.add_widget(show_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def show_books(self, instance):
+        books = library.get_books_not_borrowed()
+        self.result.text = '\n'.join([f"{b[0]}: {b[1]} by {b[2]}" for b in books])
+
+class AddLenderScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.name_input = TextInput(hint_text='Name')
+        self.address_input = TextInput(hint_text='Address')
+        self.mobile_input = TextInput(hint_text='Mobile')
+        add_btn = Button(text='Add Lender')
+        self.result = Label(text='')
+        add_btn.bind(on_release=self.add_lender)
+        layout.add_widget(self.name_input)
+        layout.add_widget(self.address_input)
+        layout.add_widget(self.mobile_input)
+        layout.add_widget(add_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def add_lender(self, instance):
+        name = self.name_input.text
+        address = self.address_input.text
+        mobile = self.mobile_input.text
+        if name:
+            lendor_id = library.add_lender(name, address, mobile)
+            self.result.text = f"Lender added with ID: {lendor_id}"
+        else:
+            self.result.text = "Please enter lender name."
+
+class RemoveLenderScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.lender_id_input = TextInput(hint_text='Lender ID')
+        remove_btn = Button(text='Remove Lender')
+        self.result = Label(text='')
+        remove_btn.bind(on_release=self.remove_lender)
+        layout.add_widget(self.lender_id_input)
+        layout.add_widget(remove_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def remove_lender(self, instance):
+        try:
+            lendor_id = int(self.lender_id_input.text)
+            library.remove_lender(lendor_id)
+            self.result.text = f"Lender {lendor_id} removed."
+        except Exception as e:
+            self.result.text = str(e)
+
+class BorrowBookScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.lender_id_input = TextInput(hint_text='Lender ID')
+        self.book_id_input = TextInput(hint_text='Book ID')
+        borrow_btn = Button(text='Borrow Book')
+        self.result = Label(text='')
+        borrow_btn.bind(on_release=self.borrow_book)
+        layout.add_widget(self.lender_id_input)
+        layout.add_widget(self.book_id_input)
+        layout.add_widget(borrow_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def borrow_book(self, instance):
+        try:
+            lendor_id = int(self.lender_id_input.text)
+            book_id = int(self.book_id_input.text)
+            borrowed_id = library.borrow_book(lendor_id, book_id)
+            self.result.text = f"Book {book_id} borrowed by lender {lendor_id}. Borrowed ID: {borrowed_id}"
+        except Exception as e:
+            self.result.text = str(e)
+
+class ReturnBookScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.borrowed_id_input = TextInput(hint_text='Borrowed ID')
+        return_btn = Button(text='Return Book')
+        self.result = Label(text='')
+        return_btn.bind(on_release=self.return_book)
+        layout.add_widget(self.borrowed_id_input)
+        layout.add_widget(return_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def return_book(self, instance):
+        try:
+            borrowed_id = int(self.borrowed_id_input.text)
+            library.return_borrowed_book(borrowed_id)
+            self.result.text = f"Book with borrowed ID {borrowed_id} returned."
+        except Exception as e:
+            self.result.text = str(e)
+
+class BorrowedBooksLenderScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.result = Label(text='')
+        show_btn = Button(text='Show Borrowed Books with Lender Details')
+        show_btn.bind(on_release=self.show_borrowed)
+        layout.add_widget(show_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def show_borrowed(self, instance):
+        books = library.get_books_borrowed_with_lender_details()
+        self.result.text = '\n'.join([f"Book {b[0]}: {b[1]} by {b[2]}, Lender: {b[3]}, {b[4]}, {b[5]}, Borrowed ID: {b[6]}" for b in books])
+
+class BooksNotBorrowedScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        self.result = Label(text='')
+        show_btn = Button(text='Show Books Not Borrowed')
+        show_btn.bind(on_release=self.show_books)
+        layout.add_widget(show_btn)
+        layout.add_widget(self.result)
+        layout.add_widget(Button(text='Back', on_release=lambda x: setattr(self.manager, 'current', 'main_menu')))
+        self.add_widget(layout)
+    def show_books(self, instance):
+        books = library.get_books_not_borrowed()
+        self.result.text = '\n'.join([f"{b[0]}: {b[1]} by {b[2]}" for b in books])
+
+class PersonalLibraryApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainMenu(name='main_menu'))
+        sm.add_widget(AddBookScreen(name='add_book'))
+        sm.add_widget(RemoveBookScreen(name='remove_book'))
+        sm.add_widget(ShowAllBooksScreen(name='show_all_books'))
+        sm.add_widget(ShowAvailableBooksScreen(name='show_available_books'))
+        sm.add_widget(AddLenderScreen(name='add_lender'))
+        sm.add_widget(RemoveLenderScreen(name='remove_lender'))
+        sm.add_widget(BorrowBookScreen(name='borrow_book'))
+        sm.add_widget(ReturnBookScreen(name='return_book'))
+        sm.add_widget(BorrowedBooksLenderScreen(name='borrowed_books_lender'))
+        sm.add_widget(BooksNotBorrowedScreen(name='books_not_borrowed'))
+        return sm
+
 if __name__ == '__main__':
-    lib = PersonalLibrary()
-    # Add books
-    b1 = lib.add_book('The Hobbit', 'J.R.R. Tolkien')
-    b2 = lib.add_book('1984', 'George Orwell')
-    # Add lenders
-    l1 = lib.add_lender('Alice', '123 Main St', '555-1234')
-    l2 = lib.add_lender('Bob', '456 Elm St', '555-5678')
-    # Borrow a book
-    borrow_id1 = lib.borrow_book(l1, b1)
-    print('Books borrowed:')
-    for row in lib.get_books_borrowed_with_lender_details():
-        print(row)
+    run_ui = True
+    if run_ui:
+        PersonalLibraryApp().run()
+    else:
+        lib = PersonalLibrary()
+        # Add books
+        b1 = lib.add_book('The Hobbit', 'J.R.R. Tolkien')
+        b2 = lib.add_book('1984', 'George Orwell')
+        # Add lenders
+        l1 = lib.add_lender('Alice', '123 Main St', '555-1234')
+        l2 = lib.add_lender('Bob', '456 Elm St', '555-5678')
+        # Borrow a book
+        borrow_id1 = lib.borrow_book(l1, b1)
+        print('Books borrowed:')
+        for row in lib.get_books_borrowed_with_lender_details():
+            print(row)
 
-    borrow_id2 = lib.borrow_book(l2, b2)
-    print('Books borrowed:')
-    for row in lib.get_books_borrowed_with_lender_details():
-        print(row)
+        borrow_id2 = lib.borrow_book(l2, b2)
+        print('Books borrowed:')
+        for row in lib.get_books_borrowed_with_lender_details():
+            print(row)
 
-    print('Books available:')
-    for row in lib.get_books_not_borrowed():
-        print(row)
-    # Return the book
-    lib.return_borrowed_book(borrow_id1)
-    print('Books available:')
-    for row in lib.get_books_not_borrowed():
-        print(row)
-    print(borrow_id1, borrow_id2)
-    lib.return_borrowed_book(borrow_id2)
-    print('Books available:')
-    for row in lib.get_books_not_borrowed():
-        print(row)
+        print('Books available:')
+        for row in lib.get_books_not_borrowed():
+            print(row)
+        # Return the book
+        lib.return_borrowed_book(borrow_id1)
+        print('Books available:')
+        for row in lib.get_books_not_borrowed():
+            print(row)
+        print(borrow_id1, borrow_id2)
+        lib.return_borrowed_book(borrow_id2)
+        print('Books available:')
+        for row in lib.get_books_not_borrowed():
+            print(row)
 
-    # Most/least borrowed
-    print('Most borrowed:', lib.get_most_borrowed_book())
-    print('Least borrowed:', lib.get_least_borrowed_book())
-    lib.close()
+        # Most/least borrowed
+        print('Most borrowed:', lib.get_most_borrowed_book())
+        print('Least borrowed:', lib.get_least_borrowed_book())
+        lib.close()
